7baf9a2c16d5ed036e822e69fdbfc4a1e4beaddb
Miehau <michal.mlak.1@gmail.com>
2019-10-27 18:45:46 +0000

    Edit todos table to handle 'isDone'. Modify colour on the go to mark completed tasks. Show modal on 'Edit' button. Add new task button now actually adds new row in the table

--- src/dashboard/navbar.js
+++ src/dashboard/navbar.js
@@ -5,32 +5,22 @@ import Dashboard from './dashboard';
 import ToDos from './todos'
 
 class Navbar extends React.Component {
-    state={
-        todos: []
-    }
-    componentDidMount(){
-        fetch("http://localhost:8080/tasks/all")
-        .then(res => res.json())
-        .then(data => {
-            this.setState({todos: data})
-        })
-        .catch(console.log)
-    }
+
 
     render() {
         return (
             <Router>
-            <div className="ui attached labeled icon menu">
+                <div className="ui attached labeled icon menu">
                     <div className="left menu">
                         <NavLink to="/dashboard" className="item" activeClassName="active"><i className="braille icon" />Dashboard</NavLink>
-                        <NavLink to="/todo/" className="item" activeClassName="active"><i className="tasks icon"/>ToDos</NavLink>
+                        <NavLink to="/todo/" className="item" activeClassName="active"><i className="tasks icon" />ToDos</NavLink>
                     </div>
-            </div>
-            <Route path="/" exact> <Redirect to="/dashboard"/></Route>
-            <Route path="/dashboard" component={Dashboard}/>
-            <Route path="/todo/"  render={props => (
-                <ToDos {...props} todos={this.state.todos}/>
-            )} />
+                </div>
+                <Route path="/" exact> <Redirect to="/dashboard" /></Route>
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/todo/" render={props => (
+                    <ToDos {...props} />
+                )} />
             </Router>
         );
     }
--- src/dashboard/todos.js
+++ src/dashboard/todos.js
@@ -1,39 +1,134 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuid from 'uuid/v4';
+import { Button, Header, Image, Modal, Form, Checkbox } from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
 
+class TableRow extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: props.task,
+            modalOpen: false,
+            handleEdit: props.onClick
+        }
+        this.openModal = this.openModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.markDone = this.markDone.bind(this);
+    }
+    openModal() {
+        this.setState({ modalOpen: true });
+    }
+    handleSubmit(e) {
+        console.log(e.target);
+        this.state.handleEdit(this.state.task);
+        this.setState({ modalOpen: false });
+    }
+
+    markDone() {
+        var t = this.state.task;
+        t.done = true;
+        this.setState({ task: t });
+        this.state.handleEdit(this.state.task);
+    }
+
+    render() {
+        return (
+            <tr className={this.state.task.done ? 'positive' : null}>
+                <td>{this.state.task.name} </td>
+                <td>{this.state.task.description}</td>
+                <td>{this.state.task.done ? "Finished" : "Not finished"}</td>
+                <td>
+                    <Modal open={this.state.modalOpen} trigger={<Button icon='edit' onClick={this.openModal} basic color='blue' />}>
+                        <Modal.Header>Edit task</Modal.Header>
+                        <Modal.Content>
+                            <Modal.Description>
+                                <Form onSubmit={this.state.handleEdit}>
+                                    <Form.Group widths='equal'>
+                                        <Form.Field
+                                            control={Input}
+                                            label='Name'
+                                            defaultValue={this.state.task.name}
+                                        />
+                                        <Form.Field
+                                            control={Input}
+                                            label='Description'
+                                            defaultValue={this.state.task.description}
+                                        />
+                                    </Form.Group>
+                                    <Form.Field onClick={this.handleSubmit} control={Button}>Submit</Form.Field>
+                                </Form>
+                            </Modal.Description>
+                        </Modal.Content>
+                    </Modal>
+                    <Button icon='checkmark' onClick={this.markDone} basic color='green' />
+                </td>
+            </tr>
+        );
+    }
+}
 
 class ToDos extends React.Component {
     constructor(props) {
         super(props);
-        if(props.todos && props.todos.data){
-            this.state = { todos: props.todos };
-        }
-        else{
-            this.state = {todos: {data:[]}}
-        }
-        console.log(props);
+        this.state = { todos: { data: [] } }
+        this.addTask = this.addTask.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+    }
+    componentDidMount() {
+        fetch("http://localhost:8080/tasks/all")
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ todos: data.data });
+            })
+            .catch(console.log)
+    }
+    handleEdit(e) {
+        console.log("Dupa");
+        console.log(e);
     }
     getTableTodos() {
         let rows = [];
-        for (let i = 0; i < this.state.todos.data.length; i++) {
-            let task = this.state.todos.data[i].attributes.task;
+        for (let i = 0; i < this.state.todos.length; i++) {
+            let task = this.state.todos[i].attributes.task;
             rows.push(
-                <tr className={this.isDone(task)} key={task.id}>
-                    <td>{task.name}</td>
-                    <td>{task.description}</td>
-                    <td>{task.done ? "Finished" : "Not finished"}</td>
-                </tr>
+                <TableRow key={task.id} onClick={this.handleEdit} isEditable={false} task={task} />
             );
         }
+        console.log(rows);
         return rows;
     }
     isDone(task) {
         return task.done ? "positive" : "";
     }
 
+
+    addTask = (event) => {
+        console.log(this.state);
+        var newId = uuid();
+        var joined = this.state.todos.concat(
+            {
+                attributes: {
+                    task: {
+                        id: newId,
+                        name: "New tasks name",
+                        description: "New task Descriptopn",
+                        done: false
+                    }
+                }
+            }
+        );
+        this.setState({
+            todos: joined
+        })
+    }
+
     render() {
         return (
             <div className="ui raised padded container segment">
+
+
                 <div>
                     <table className="ui selectable celled table">
                         <thead>
@@ -41,23 +136,32 @@ class ToDos extends React.Component {
                                 <th>Name</th>
                                 <th>Status</th>
                                 <th>Notes</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>John</td>
-                                <td>No Action</td>
-                                <td>None</td>
-                            </tr>
                             {this.getTableTodos()}
                         </tbody>
                         <tfoot className="fullwidth">
                             <tr>
                                 <th></th>
                                 <th colSpan="4">
-                                    <div className="ui right floated small primary labeled icon button">
+                                    <Modal trigger={<div onClick={this.addTask} className="ui right floated small primary labeled icon button">
                                         <i className="list icon"></i> Add item
-                                    </div>
+                                    </div>}>
+                                        <Modal.Header>Select a Photo</Modal.Header>
+                                        <Modal.Content image>
+                                            <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
+                                            <Modal.Description>
+                                                <Header>Default Profile Image</Header>
+                                                <p>
+                                                    We've found the following gravatar image associated with your e-mail
+                                                    address.
+                                                </p>
+                                                <p>Is it okay to use this photo?</p>
+                                            </Modal.Description>
+                                        </Modal.Content>
+                                    </Modal>
                                 </th>
                             </tr>
                         </tfoot>
@@ -67,5 +171,6 @@ class ToDos extends React.Component {
         )
     }
 }
+
 
 export default ToDos;
